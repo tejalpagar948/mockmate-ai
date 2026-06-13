c0e075d6eea05c7dd040f491ce56adfabadc0a1e
tejalpagar948 <tejalpagar948@gmail.com>
2026-06-13 21:33:19 +0530

    configure drizzle orm

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'drizzle-kit';
+import 'dotenv/config';
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './utils/schema.js',
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
+  },
+});
