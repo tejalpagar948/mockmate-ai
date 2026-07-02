fcd58c8940ac03353a5db371846041b2b403b010
tejalpagar948 <tejalpagar948@gmail.com>
2026-07-02 23:39:54 +0530

    added gemini api code

---
+++ utils/GeminiAIModal.tsx
@@ -0,0 +1,47 @@
+// To run this code you need to install the following dependencies:
+// npm install @google/genai mime
+// npm install -D @types/node
+
+import { GoogleGenAI, ThinkingLevel } from '@google/genai';
+
+async function main() {
+  const ai = new GoogleGenAI({
+    apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+  });
+  const tools = [
+    {
+      googleSearch: {},
+    },
+  ];
+  const config = {
+    thinkingConfig: {
+      thinkingLevel: ThinkingLevel.HIGH,
+    },
+    tools,
+  };
+  const model = 'gemini-3-flash-preview';
+  const contents = [
+    {
+      role: 'user',
+      parts: [
+        {
+          text: `INSERT_INPUT_HERE`,
+        },
+      ],
+    },
+  ];
+
+  const response = await ai.models.generateContentStream({
+    model,
+    config,
+    contents,
+  });
+  let fileIndex = 0;
+  for await (const chunk of response) {
+    if (chunk.text) {
+      console.log(chunk.text);
+    }
+  }
+}
+
+main();
